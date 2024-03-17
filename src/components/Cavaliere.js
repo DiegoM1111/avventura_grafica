cae38610ceede0f426f421a75d599f8767ff89b6
DiegoM1111 <diegomilli03@gmail.com>
2024-03-17 19:31:38 +0100

    cavaliere

---
+++ src/components/Cavaliere.js
@@ -0,0 +1,15 @@
+import '../assets/cavaliere.css';
+
+function Cavaliere(){
+    let stato = "idle";
+    // if(stato){
+
+    // }
+
+    return (<>
+        <div className={stato}></div>
+    </>
+    );
+}
+export default Cavaliere;
+    
