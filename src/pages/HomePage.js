a2f4620de1ce7e55fc9460d585905fdf96061d80
DiegoM1111 <diegomilli03@gmail.com>
2024-03-17 10:51:00 +0100

    ...

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import Riquadro from "../components/Riquadro";
 import Bottone from '../components/Bottone';
 import '../App.css';
+// import background from '../materiale/image3.png';
 
 function HomePage() {
+    // const containerStyle = {
+    //     backgroundImage: `url(${background})`,
+    //     width: "100%",
+    //     margin: "0 auto",
+    // };
+
     return (
         <>
             <div className="separatore"></div>
             
-            <div className="container sfondo">
+            <div className="container">
+            {/* <div style={containerStyle}> */}
                 
                 <div className="row">
                     <div className="col-4"></div>
